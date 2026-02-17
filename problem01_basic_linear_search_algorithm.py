8f0c16cf696a38abc90f4e06891e6eae05b61fa5
Sanchit <sanchit10gawde@gmail.com>
2026-02-17 10:43:56 +0530

    DSA

--- problem01_basic_linear_search_algorithm.py
+++ problem01_basic_linear_search_algorithm.py
@@ -1,5 +1,21 @@
 """"
 Alice has some cards with numbers written on them.
-She Arranges the cards in descreasing order
+She arranges the cards in decreasing order and lays them out face down in a sequence on a table.
+She challenges BOB to pick out the containing a given number by turning over as few cards as possible
+Write a function to help Bob locate the card as quickly as possible
 
+input : list_of_cards, card_to_pick
+output: If card is found then print the card to pick if not found print -1
+
+Edge Cases:
+
+1) List can be empty -1
+2) List wont have the card -1
+
+Test-Cases
+3) It may be the start of LHS
+4) It may be end of LHS
 """
+
+# BRUTE FORCE SOLUTION
+
